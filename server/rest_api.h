0fe068818d4057b71d8e85e637159f14306a60b8
latter2005 <45479174+latter2005@users.noreply.github.com>
2020-06-07 23:05:10 +0900

    셈플 테스트 수행용

--- server/rest_api.h
+++ server/rest_api.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
-
-class rest_api
-{
+#pragma comment(lib, "cpprest142_2_10")
+#pragma comment(lib, "cpprest142_2_10d")
+class rest_api{
 public:
+	static void get_http();
+	static void get_http_async();
+	static void get_json();
 	rest_api();
 	~rest_api();
 };
